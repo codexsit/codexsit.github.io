e13015da85748c5c27ffd4b3a4e9b15d2fa86b81
Miran <miranfirdausi027@gmail.com>
2024-02-23 09:49:31 +0530

    fix(workflow): changed workflow from linting to build only and setupped tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#232323',
+        primary: '#F24E3F',
+        text: '#F7F7F7',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
