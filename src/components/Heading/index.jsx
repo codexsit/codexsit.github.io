b1b566d2c4b2f6451e16438a7629e355217220c0
Miran Firdausi <75238590+Miran-Firdausi@users.noreply.github.com>
2024-02-27 19:51:58 +0530

    fix: responsiveness of heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 function Heading({ headingText }) {
   return (
     <div className="relative text-center font-black uppercase tracking-tighter">
-      <div className="xs:text-5xl text-[6.5rem] text-outlined text-transparent  ">
+      <div className="xs:text-4xl sm:text-6xl lg:text-[6.5rem] text-outlined text-transparent">
         {headingText}
       </div>
-      <div className="xs:text-2xl text-[3.75rem] text-foreground absolute w-full bottom-0">
+      <div className="xs:text-2xl sm:text-4xl lg:text-[3.75rem] text-foreground absolute w-full xs:bottom-[-10px] sm:bottom-[-18px] lg:bottom-[-12px]">
         {headingText}
       </div>
     </div>
