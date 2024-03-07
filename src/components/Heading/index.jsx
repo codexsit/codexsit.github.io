96f4e3c237ef0bf3f37dc15e8539f9c872f45779
GenDelta <ankush.dutta.kol@gmail.com>
2024-03-07 22:35:48 +0530

    feat: removed redundant images

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,12 +1,18 @@
 import PropTypes from "prop-types";
 
-function Heading({ text,className,bgTextStyle,frontTextStyle }) {
+function Heading({ text, className, bgTextStyle, frontTextStyle }) {
   return (
-    <div className={`relative text-center font-black uppercase tracking-tighter ${className}`}>
-      <div className={`${bgTextStyle} xs:text-4xl sm:text-6xl lg:text-8xl text-outlined text-transparent`}>
+    <div
+      className={`relative text-center font-black uppercase tracking-tighter ${className}`}
+    >
+      <div
+        className={`${bgTextStyle} xs:text-4xl sm:text-6xl lg:text-8xl text-outlined text-transparent`}
+      >
         {text}
       </div>
-      <div className={`${frontTextStyle} xs:text-2xl sm:text-4xl lg:text-6xl absolute w-full leading-none bottom-[-20%]`}>
+      <div
+        className={`${frontTextStyle} xs:text-2xl sm:text-4xl lg:text-6xl absolute w-full leading-none bottom-[-20%]`}
+      >
         {text}
       </div>
     </div>
