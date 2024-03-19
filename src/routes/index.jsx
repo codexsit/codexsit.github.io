e140cecb480a5bbd0c79f7880871a88c009d259e
Jayu1214 <jayutrivedi1214@gmail.com>
2024-03-19 18:08:18 +0530

    feat: add Gallery Page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,15 @@
 import Home from "@/pages/Home/index";
 import Teams from "@/pages/Teams/index";
 import About from "@/pages/About/index";
+import {MasonryGridGallery}from "../pages/Gallery/MasonryGridGallery";
 
 const routes = [
+  {
+    lable: "Gallery",
+    path: "/gallery",
+    requireAuth: false,
+    render: <MasonryGridGallery/>,
+  },
   {
     lable: "About",
     path: "/about-us",
