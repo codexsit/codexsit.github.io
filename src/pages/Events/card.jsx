0fb4792300e8c3522b87eedbe3dfed50f3e1d963
Abhishek Patawari <abhishekpatawari.codes@gmail.com>
2024-04-01 22:37:13 +0530

    Update card.jsx

--- src/pages/Events/card.jsx
+++ src/pages/Events/card.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function Card({ user }) {
+function Card({ events }) {
   const [truncatedText, setTruncatedText] = useState("");
   const maxLength = 25; // Maximum number of words
 
   useEffect(() => {
-    const text = user.description;
+    const text = events.description;
     const words = text.split(" ");
 
     if (words.length > maxLength) {
@@ -20,16 +20,16 @@ function Card({ user }) {
     <div className="  shadow-lg bg-zinc-600 rounded-2xl max-w-96 h-[30rem] relative">
       <img
         className="w-mag rounded-se-2xl rounded-ss-2xl"
-        src={`${user.image}`}
-        alt={user.alt}
+        src={`${events.image}`}
+        alt={events.alt}
       />
       <div className="content-box px-4 pt-2">
-        <p className="py-1 font-medium text-white break-words">{user.name}</p>
-        <p className="date font-thin py-2 pr-3 text-zinc-400">{user.date}</p>
+        <p className="py-1 font-medium text-white break-words">{events.name}</p>
+        <p className="date font-thin py-2 pr-3 text-zinc-400">{events.date}</p>
         <p className="description text-white font-light ">{truncatedText}</p>
       </div>
       <Link
-        to={user.link}
+        to={events.link}
         className="absolute bottom-0 left-0 right-0 text-center tracking-widest font-thin drop-shadow-lg text-white py-7 hover:text-red-600"
       >
         READ MORE
@@ -38,7 +38,7 @@ function Card({ user }) {
   );
 }
 Card.propTypes = {
-  user: PropTypes.isRequired,
+  events: PropTypes.isRequired,
 };
 
 export default Card;
