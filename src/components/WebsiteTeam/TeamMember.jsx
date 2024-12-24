d97972e86ded12727707f150ccfd46c09caf1da0
GenDelta <ankush.dutta.kol@gmail.com>
2024-12-24 19:39:06 +0530

    fix: changed grid to 4 columns per row

--- src/components/WebsiteTeam/TeamMember.jsx
+++ src/components/WebsiteTeam/TeamMember.jsx
@@ -13,95 +13,105 @@ import AbhishekPatawariImage from "@/assets/images/Website_Team/AbhishekPatawari
 import JayTrivediImage from "@/assets/images/Website_Team/JayTrivedi.png";
 
 const images = {
-    1: MiranFirdausiImage,
-    2: AchyutShuklaImage,
-    3: MitikshaPaliwalImage,
-    4: AnkushDuttaImage,
-    5: AbhishekPatawariImage,
-    6: JayTrivediImage,
+  1: MiranFirdausiImage,
+  2: AchyutShuklaImage,
+  3: MitikshaPaliwalImage,
+  4: AnkushDuttaImage,
+  5: AbhishekPatawariImage,
+  6: JayTrivediImage,
 };
 
 const bgimages = {
-    1: RectangleRed,
-    2: RectangleGreen,
-    3: RectangleYellow,
-    4: RectangleGreen,
-    5: RectangleRed,
-    6: RectangleYellow,
+  1: RectangleRed,
+  2: RectangleGreen,
+  3: RectangleYellow,
+  4: RectangleGreen,
+  5: RectangleRed,
+  6: RectangleYellow,
 };
-
-
 function TeamMember({ member }) {
-    const { name, batch, description, linkedin, instagram, github, id } = member;
-    const image = images[id];
+  const { name, batch, description, linkedin, instagram, github, id } = member;
+  const image = images[id];
 
-    return (
-        <div className="relative group transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-200 cursor-pointer overflow-hidden p-8 align-middle max-w-[700px]">
-            <div className="overflow-hidden p-4 align-middle">
-                <div className="relative">
-                    <img
-                        src={bgimages[id]}
-                        alt="background"
-                        className="absolute bottom-1 left-1/2 rounded-md transform -translate-x-1/2 w-56 h-56 object-cover z-0 transition-transform duration-700 group-hover:scale-105"
-                    />
-                    <img
-                        src={image}
-                        alt={name}
-                        className="relative w-72 h-72 mx-auto object-cover z-10 filter grayscale transition duration-500 group-hover:filter-none group-hover:duration-500 group-hover:ease-in-out group-hover:delay-150 group-hover:scale-110"
-                    />
-                </div>
-                <h2 className="text-4xl mt-10 font-poppins font-bold text-center tracking-wide text-text-light">
-                    {name}
-                </h2>
-                <p className="text-xl mt-3 font-poppins text-center font-semibold tracking-wide text-text-light">
-                    {batch}
-                </p>
-                <p className="text-xl font-poppins my-5 text-center text-wrap text-secondary-light">
-                    {description}
-                </p>
-                <div className="absolute -left-1 top-[40%] transform -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-500 sm:left-1 sm:top-[30%] md:left-4 md:top-[40%] lg:left-4 lg:top-[40%] xl:left-4 xl:top-[40%]">
-                    <div className="flex flex-col space-y-4">
-                        <a
-                            href={linkedin}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="text-white font-sans opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-100"
-                        >
-                            <img src={LinkedInIcon} alt="Linkedin" className="w-10 h-10 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 p-2" />
-                        </a>
-                        <a
-                            href={github}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="text-white font-sans opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-200"
-                        >
-                            <img src={GithubIcon} alt="Github" className="w-10 h-10 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 p-2" />
-                        </a>
-                        <a
-                            href={instagram}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="text-white font-sans opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-300"
-                        >
-                            <img src={InstagramIcon} alt="Instagram" className="w-10 h-10 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 p-2" />
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="relative group transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-200 cursor-pointer overflow-hidden p-8 align-middle max-w-[700px]">
+      <div className="overflow-hidden p-4 align-middle">
+        <div className="relative">
+          <img
+            src={bgimages[id]}
+            alt="background"
+            className="absolute bottom-1 left-1/2 rounded-md transform -translate-x-1/2 w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 object-cover z-0 transition-transform duration-700 group-hover:scale-105"
+          />
+          <img
+            src={image}
+            alt={name}
+            className="relative w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 lg:w-72 lg:h-72 mx-auto object-cover z-10 filter grayscale transition duration-500 group-hover:filter-none group-hover:duration-500 group-hover:ease-in-out group-hover:delay-150 group-hover:scale-110"
+          />
         </div>
-    );
+        <h2 className="text-2xl sm:text-3xl md:text-5xl mt-10 font-poppins font-semibold text-center tracking-wide text-text-light">
+          {name}
+        </h2>
+        <p className="text-lg sm:text-xl mt-3 font-poppins text-center font-semibold tracking-wide text-text-light">
+          {batch}
+        </p>
+        <p className="text-lg sm:text-xl font-poppins my-5 text-center text-wrap text-secondary-light">
+          {description}
+        </p>
+        <div className="mr-2 absolute -left-2 top-[40%] transform -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-500 sm:left-[0.5rem] sm:top-[30%] md:-left-[0.5rem] md:top-[40%] lg:-left-[0.5rem] lg:top-[40%] xl:-left-[0.5rem] xl:top-[40%]">
+          <div className="flex flex-col space-y-4">
+            <a
+              href={linkedin}
+              target="_blank"
+              rel="noreferrer"
+              className="text-white font-sans opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-100"
+            >
+              <img
+                src={LinkedInIcon}
+                alt="Linkedin"
+                className="w-8 h-8 sm:w-10 sm:h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 p-2"
+              />
+            </a>
+            <a
+              href={github}
+              target="_blank"
+              rel="noreferrer"
+              className="text-white font-sans opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-200"
+            >
+              <img
+                src={GithubIcon}
+                alt="Github"
+                className="w-8 h-8 sm:w-10 sm:h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 p-2"
+              />
+            </a>
+            <a
+              href={instagram}
+              target="_blank"
+              rel="noreferrer"
+              className="text-white font-sans opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-300"
+            >
+              <img
+                src={InstagramIcon}
+                alt="Instagram"
+                className="w-8 h-8 sm:w-10 sm:h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 p-2"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 TeamMember.propTypes = {
-    member: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        batch: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        linkedin: PropTypes.string.isRequired,
-        instagram: PropTypes.string.isRequired,
-        github: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired,
-    }).isRequired,
+  member: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    batch: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    linkedin: PropTypes.string.isRequired,
+    instagram: PropTypes.string.isRequired,
+    github: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default TeamMember;
--- src/components/WebsiteTeam/index.jsx
+++ src/components/WebsiteTeam/index.jsx
@@ -25,7 +25,7 @@ function WebsiteTeam() {
             end: "bottom 70%",
             toggleActions: "play none none reverse",
           },
-        }
+        },
       );
     });
 
@@ -41,22 +41,39 @@ function WebsiteTeam() {
   ));
 
   return (
-    <div className="min-h-fit bg-background-dark scroll-smooth" ref={sectionRef}>
+    <div
+      className="min-h-fit bg-background-dark scroll-smooth"
+      ref={sectionRef}
+    >
       <div className="flex justify-center items-center">
         <div className="flex-grow mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center p-6">
-              <div className="text-left text-xl sm:text-2xl md:text-3xl lg:text-[2.5rem] font-poppins px-2 space-y-2">
-                <div>
-                  <span className="text-text-websiteteamlight font-thin">Special Thanks</span>
-                  <span className="text-text-websiteteamlight font-thin"> to the</span>
-                  <span className="text-primary font-semibold"> Awesome Team</span>
-                </div>
-                <div>
-                  <span className="text-text-websiteteamlight font-thin"> Who Helped Create this</span>
-                  <span className="text-text-websiteteamyellow font-semibold"> Amazing Website</span>
-                </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-center items-center p-6">
+            <div className="text-left text-xl sm:text-3xl md:text-3xl lg:text-4xl font-poppins px-2 space-y-2">
+              <div>
+                <span className="text-text-websiteteamlight font-thin">
+                  Special Thanks
+                </span>
+                <span className="text-text-websiteteamlight font-thin">
+                  {" "}
+                  to the
+                </span>
+                <span className="text-primary font-semibold">
+                  {" "}
+                  Awesome Team
+                </span>
               </div>
-              {teamMembers}
+              <div>
+                <span className="text-text-websiteteamlight font-thin">
+                  {" "}
+                  Who Helped Create this
+                </span>
+                <span className="text-text-websiteteamyellow font-semibold">
+                  {" "}
+                  Amazing Website
+                </span>
+              </div>
+            </div>
+            {teamMembers}
           </div>
         </div>
       </div>
